cf88ead92d98ce01ae1f3ecb5f13602df8dc58b1
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2025-09-30 19:36:38 +0100

    feat: :sparkles: Class bookCatalog done

--- src/org/ulpgc/is1/model/BookCatalog.java
+++ src/org/ulpgc/is1/model/BookCatalog.java
@@ -7,9 +7,60 @@ public class BookCatalog {
     private final ArrayList<Book> bookList;
     private final ArrayList<Author> authorList;
 
-    public BookCatalog(){
-        bookList = new ArrayList();
-        authorList = new ArrayList();
+    public BookCatalog() {
+        bookList = new ArrayList<>();
+        authorList = new ArrayList<>();
     }
 
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    public ArrayList<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void addBook(String title, String editor, int year, Subject subject, Author author) {
+        Book newBook = new Book(title, editor, year, subject, author);
+        bookList.add(newBook);
+    }
+
+    public Book getBook(int index) {
+        if (index >= 0 && index < bookList.size()) {
+            return bookList.get(index);
+        }
+        return null;
+    }
+
+    public void removeBook(int index) {
+        if (index >= 0 && index < bookList.size()) {
+            bookList.remove(index);
+        }
+    }
+
+    public int bookCount() {
+        return bookList.size();
+    }
+
+    public void addAuthor(String name, String surname) {
+        Author newAuthor = new Author(name, surname);
+        authorList.add(newAuthor);
+    }
+
+    public Author getAuthor(int index) {
+        if (index >= 0 && index < authorList.size()) {
+            return authorList.get(index);
+        }
+        return null;
+    }
+
+    public void removeAuthor(int index) {
+        if (index >= 0 && index < authorList.size()) {
+            authorList.remove(index);
+        }
+    }
+
+    public int authorCount() {
+        return authorList.size();
+    }
 }
